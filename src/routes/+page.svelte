91be209d8ac3062a681c1c1184d1170bc6acc958
paradoxicalinsomniac <81902126+micolarighi@users.noreply.github.com>
2025-09-06 00:07:16 +0700

    update harga

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,9 +18,9 @@
 		{ name: 'Plat Komori (560 x 670)', price: 48000 },
 		{ name: 'Plat Oliver 72 (605 x 724)', price: 55000 },
 		{ name: 'Plat SM 72 ( 615 x 724)', price: 56000 },
-		{ name: 'Film pos-neg(A4)', price: 32000 },
-		{ name: 'Film pos-net(/cm)', price: 50 },
-		{name : 'Film pos-net(custom)', price: 1000},
+		{ name: 'Film pos-neg(A4)', price: 40000 },
+		{ name: 'Film pos-net(/cm)', price: 60 },
+		{ name: 'Film pos-net(custom)', price: 1000 }
 	];
 	let currentSelectedItem = 0;
 	let quantity = 1;
@@ -39,33 +39,33 @@
 		quantity = qtt.detail;
 	}
 	function handleClick() {
-
 		if (currentSelectedItem == 10) {
-			let customPrice = prompt("Masukkan Harga Custom (Akan dikali 1000) : ")
+			let customPrice = prompt('Masukkan Harga Custom (Akan dikali 1000) : ');
 			if (customPrice.length < 1) {
-				alert("Masukkan Harga Custom!")
+				alert('Masukkan Harga Custom!');
 			} else {
-			addedItems = [
-			...addedItems,
-			[	
-				itemList[10].name,
-				itemList[10].price = parseFloat(customPrice) * 1000,
-				quantity,
-				parseInt(itemList[10].price) * quantity
-			]
-		];}
-		toast.push('Barang berhasil ditambah!');
+				addedItems = [
+					...addedItems,
+					[
+						itemList[10].name,
+						(itemList[10].price = parseFloat(customPrice) * 1000),
+						quantity,
+						parseInt(itemList[10].price) * quantity
+					]
+				];
+			}
+			toast.push('Barang berhasil ditambah!');
 		} else {
 			addedItems = [
-			...addedItems,
-			[	
-				itemList[currentSelectedItem].name,
-				itemList[currentSelectedItem].price,
-				quantity,
-				parseInt(itemList[currentSelectedItem].price) * quantity
-			]
-		];
-		toast.push('Barang berhasil ditambah!');
+				...addedItems,
+				[
+					itemList[currentSelectedItem].name,
+					itemList[currentSelectedItem].price,
+					quantity,
+					parseInt(itemList[currentSelectedItem].price) * quantity
+				]
+			];
+			toast.push('Barang berhasil ditambah!');
 		}
 	}
 	function handleShowReceipt() {
